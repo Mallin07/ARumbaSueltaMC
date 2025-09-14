16ba9e3aa794362f1951c2e0580ad4d75a499aaa
Marc <marc.prof.musica@gmail.com>
2025-09-14 13:29:38 +0200

    jjioj

--- principal/principal.js
+++ principal/principal.js
@@ -1,5 +1,24 @@
+// auth-guard.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-const auth = getAuth();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASxL2FjzUdP0Lm5pzfV5xmA1nywPMWdjw",
+  authDomain: "arsmc-873f3.firebaseapp.com",
+  projectId: "arsmc-873f3",
+  storageBucket: "arsmc-873f3.appspot.com",
+  messagingSenderId: "558517245585",
+  appId: "1:558517245585:web:9816b346b67c4d7cb69130",
+  measurementId: "G-8WGXBDMSVZ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Si no hay usuario, sacamos al login (index.html)
 onAuthStateChanged(auth, (user) => {
-  if (!user) window.location.href = "../index.html";
+  if (!user) {
+    // Ajusta la ruta si tu index está en otra carpeta
+    window.location.href = "../index.html";
+  }
 });
